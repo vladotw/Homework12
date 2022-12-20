867cee987095f3246a8ae82f1759d21563ec5eb1
Vlad <vladot@ya.ru>
2022-12-20 19:15:10 +0600

    Homework

--- src/pro/sky/java/course1/HW12/Main.java
+++ src/pro/sky/java/course1/HW12/Main.java
@@ -7,8 +7,6 @@ public class Main {
         Author darkStarAuthor = new Author("Вера", "Камша");
         System.out.println("itAuthor.getAuthorName() = " + itAuthor.getAuthorName());
         System.out.println("darkStarAuthor.getAuthorName() = " + darkStarAuthor.getAuthorName());
-        String itAuthorNew = itAuthor.getAuthorName();
-        System.out.println("itAuthorNew = " + itAuthorNew);
 
         Book bookIt = new Book("IT", itAuthor, 1986);
         System.out.println("bookIt.bookTitle = " + bookIt.getBookTitle());
@@ -17,7 +15,7 @@ public class Main {
 
         Book bookDarkStar = new Book("Тёмная звезда", darkStarAuthor, 2001);
         System.out.println("bookDarkStar.bookTitle = " + bookDarkStar.getBookTitle());
-        System.out.println("bookDarkStar.bookName = " + bookDarkStar.getBookAuthorName());
+        System.out.println("bookDarkStar.bookAuthor = " + bookDarkStar.getBookAuthorName());
         System.out.println("bookDarkStar.publicationYear = " + bookDarkStar.getPublicationYear());
     }
 
